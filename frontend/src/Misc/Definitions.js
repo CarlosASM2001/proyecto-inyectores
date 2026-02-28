44ed4b2a2b98809b64df33641ed52f1658a3d2f5
Jose Briceño <gregorio1502@hotmail.com>
2026-02-28 11:36:57 -0400

    feat(ui,service): rediseñar modal de servicios con gestión mejorada de productos
    - Implementa búsqueda en tiempo real de repuestos con spinner de carga
    - Sustituye componente externo por controles propios (+/-, input manual, eliminar)
    - Añade utilidad CSS global para ocultar flechas de inputs numéricos
    - Reestructura interfaz con header fijo, scroll inteligente y diseño responsive
    - Mejora validaciones de cantidad y previene duplicados en la lista

--- frontend/src/Misc/Definitions.js
+++ frontend/src/Misc/Definitions.js
@@ -2,3 +2,5 @@ export const T_Ser = "Service";
 export const T_Pro = "Product";
 export const Spli_Delimitador = "|_";
 
+export const button_Imput_none =
+  " [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [-moz-appearance:textfield]";
